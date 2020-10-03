0fc0b1bf831d7f32fd05d1a74b5c7c9435dd6f11
joa <xingyu.znou@gmail.com>
2020-10-04 02:26:05 +0900

    print jenkins password

--- create.sh
+++ create.sh
@@ -4,3 +4,4 @@ vagrant up
 echo 'git config --global user.email "xingyu.zznou@gmail.com"' | vagrant ssh
 echo 'git config --global user.name "joa"' | vagrant ssh
 echo 'git clone https://github.com/xingyu-zhou/ansible-playbook-sample.git && cd ansible-playbook-sample && ansible-playbook -i development site.yml --diff' | vagrant ssh
+echo 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword' | vagrant ssh
